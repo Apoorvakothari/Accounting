8226f94665dcd1fbcd2c0bcfd653dc1ebceaa31f
Apoorva Kothari <lit2021029@iiitl.ac.in>
2023-10-11 00:24:18 +0530

    income and expense filters

---
+++ client/src/components/income-filter-form/index.jsx
@@ -0,0 +1,84 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+
+const FilterForm = ({
+  categories,
+  selectedCategory,
+  setSelectedCategory,
+  setSelectedDateRange,
+}) => {
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+
+  function handleCategoryChange(event) {
+    const category = event.target.value;
+    setSelectedCategory(category);
+  }
+
+  function handleStartDateChange(event) {
+    const date = event.target.value;
+    setStartDate(date);
+    setSelectedDateRange((prevDateRange) => ({
+      ...prevDateRange,
+      startDate: date,
+    }));
+  }
+
+  function handleEndDateChange(event) {
+    const date = event.target.value;
+    setEndDate(date);
+    setSelectedDateRange((prevDateRange) => ({
+      ...prevDateRange,
+      endDate: date,
+    }));
+  }
+
+  return (
+    <>
+      <form className="filter-form">
+        <div>
+          <label htmlFor="category">Category</label>
+          <select
+            id="category"
+            name="category"
+            value={selectedCategory}
+            onChange={handleCategoryChange}
+          >
+            {categories.map((category, index) => (
+              <option key={category} value={index === 0 ? "" : category}>
+                {category}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label htmlFor="start-date">Start Date</label>
+          <input
+            id="start-date"
+            type="date"
+            value={startDate}
+            onChange={handleStartDateChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="end-date">End Date</label>
+          <input
+            id="end-date"
+            type="date"
+            value={endDate}
+            onChange={handleEndDateChange}
+          />
+        </div>
+      </form>
+    </>
+  );
+};
+
+FilterForm.propTypes = {
+  categories: PropTypes.array.isRequired,
+  selectedCategory: PropTypes.string.isRequired,
+  setSelectedCategory: PropTypes.func.isRequired,
+  setSelectedDateRange: PropTypes.func.isRequired,
+};
+
+export default FilterForm;
