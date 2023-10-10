bad6376c36089cea8aceb2f72e1983313ca125e1
Apoorva Kothari <lit2021029@iiitl.ac.in>
2023-10-10 23:28:06 +0530

    transactions

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -3,14 +3,15 @@ import { Route, Routes } from "react-router-dom";
 
 import Navigation from "../../components/Navigation/Navigation";
 import { getUser } from "../../utilities/users-service";
-import AuthPage from "../AuthPage/AuthPage";
-import DashboardPage from "../DashboardPage/DashboardPage";
-import ExpensesPage from "../ExpensesPage/ExpensesPage";
-import IncomesPage from "../IncomesPage/IncomesPage";
-import LandingPage from "../LandingPage/LandingPage";
-import TransactionsPage from "../TransactionsPage/TransactionsPage";
 
-import "./App.css";
+import AuthPage from "./pages/auth/auth.page";
+import DashboardPage from "./pages/dashboard/dashboard.page";
+import ExpensesPage from "./pages/expense/expense.page";
+import IncomesPage from "./pages/income/income.pages";
+import LandingPage from "./pages/home/home.page";
+import TransactionsPage from "./pages/transaction/transaction.page";
+
+import "./app.css";
 
 const App = () => {
   const [user, setUser] = useState(getUser());
